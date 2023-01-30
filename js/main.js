767a3ff0728aec8af784798bfd0d7d6279db4960
Vomkt <eugene.byba@gmail.com>
2023-01-30 22:49:39 +0300

    add slider

--- js/main.js
+++ js/main.js
@@ -32,3 +32,37 @@ $(document).ready(function(){
           ]
     });
 });
+
+
+$(document).ready(function(){
+  $('.testimonials__inner').slick({
+      infinite: true,
+      slidesToShow: 2,
+      arrows: true,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      appendArrows: '.decbase-arrows',
+      prevArrow: '.decbase-arrows__btn--prev',
+      nextArrow: '.decbase-arrows__btn--next',
+      responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+  });
+});
